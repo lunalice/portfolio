5eb58fe82081489a0baedd8a2388da4efadc1124
lunalice <yamashita0120@gmail.com>
2020-07-15 17:58:52 +0900

    Modify javascript for Gallery

---
+++ created_images.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# 毎回htmlを触りたくないのでデータtxtにしてちょっと楽する、アプリならタスクにしておけば良きかも
+# 後はjavascriptをonloadでいろいろします
+
+ls -1 docs/img/gallery > docs/data/gallery.txt
--- docs/js/application.js
+++ docs/js/application.js
@@ -1,7 +1,22 @@
 // 暫定でjqueryでtopにdummyを置く
 $(function () {
-  var height = $("header").height();
+  let height = $("header").height();
   $(".hero").css("margin-top", height);
   $("main").css("margin-top", height + 30);
   $("main").css("margin-bottom", height + 10);
+});
+
+// Galleryで画像の読み込み
+$(function () {
+  let gallery = $(".gallery");
+  if (gallery) {
+    $.get("./data/gallery.txt", function (data) {
+      let lines = data.split(/\r?\n/);
+      lines.forEach(line => {
+        if (line !== '') {
+          gallery.append(`<img class="gallery-item column col-4" src="img/gallery/${line}">`);
+        }
+      });
+    });
+  }
 });
